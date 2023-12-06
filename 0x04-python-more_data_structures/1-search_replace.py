c86b2bf50a18bfc7bafd787e09dfa37c2bd7ddda
Zainoo123 <zaisarhane123@gmail.com>
2023-12-06 09:50:21 -0800

    Search and replace

---
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+def search_replace(my_list, search, replace):
+    new_list = []
+    if my_list is None:
+        return (None)
+    if len(my_list) == 0:
+        return ([])
+    for i in my_list:
+        if i == search:
+            new_list.append(replace)
+        else:
+            new_list.append(i)
+    return (new_list)
