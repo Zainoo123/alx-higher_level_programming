43cad975063227facb0d344e9d707273a1e4b3cb
Zainoo123 <zaisarhane123@gmail.com>
2024-03-17 16:19:10 -0700

    fix$

--- 0x0F-python-object_relational_mapping/model_city.py
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
 
 """
-    this module contains a Base and State class
+    this module contains a Base and City class
 """
 
 from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import ForeignKey
 from sqlalchemy import Column
 from sqlalchemy import String
 from sqlalchemy import Integer
@@ -13,14 +14,16 @@ from sqlalchemy import Integer
 Base = declarative_base()
 
 
-class State(Base):
+class City(Base):
     """
-        State class inherits the Base class
+        City class inherits the Base class
         Attributes:
             id (int)
             name (string)
+            state_id (string)
     """
-    __tablename__ = 'states'
+    __tablename__ = 'cities'
 
     id = Column(Integer, primary_key=True, autoincrement=True, nullable=False)
     name = Column(String(128), nullable=False)
+    state_id = Column(Integer, ForeignKey('states.id'), nullable=False)
